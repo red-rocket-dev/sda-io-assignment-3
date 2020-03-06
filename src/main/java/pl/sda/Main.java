cd9ea24262bc9a8d70ad30c3db0d6484eb77eba0
Jakub <kubplon@gmail.com>
2020-03-07 00:46:06 +0100

    Notebooks assignment

---
+++ src/main/java/pl/sda/Main.java
@@ -0,0 +1,33 @@
+package pl.sda;
+
+public class Main {
+    public static void main(String[] args) {
+        /*
+        1. Czytaj plik notebooks.txt linijka po linijce i wyświetlaj w konsoli dla każdej linijki:
+        Marka: <marka zawarta w danej linijce>
+        Model: <model zawarty w danej linijce>
+        Cena: <cena zawarta w danej linijce>
+        zauwaz że struktura pliku to: marka;model;cena
+        2. W osobnym pliku utworz klase Notebook majaca pola prywatne:
+        - brand (String)
+        - model (String)
+        - price (jakiego typu tu najlepiej uzyc?)
+        - to podanych pól utworz:
+            - gettery
+            - settery
+            - konstruktor domyślny
+            - konstruktor przepisujący wszystkie pola
+        3. W metodzie main utworz listę, która będzie przechowywała obiekty typu Notebook
+        4. Zamiast wypisywać poszczególne rzeczy na konsole za każdym razem kiedy czytasz linijkę:
+            - Utwórz nowy obiekt typu notebook
+            - Przypisz marke do pola brand, model do pola model i cene do pola price
+            - Dodaj obiekt do wcześniej utworzonej listy
+        5. Po przeczytaniu pliku i wypełnieniu listy:
+            - sprawdź czy na pewno lista zawiera wszystkie elementy z pliku (możesz na przykład w debugu)
+            - w liście znajdź:
+                - najdroższego laptopa
+                - średnią cenę laptopów
+                - laptopów jakiej marki jest w niej najwięcej
+         */
+    }
+}
